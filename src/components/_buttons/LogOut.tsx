4aa465c6bb261f7b7b701ff9c4cd7a73b9129f5a
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-09-17 16:29:03 +0300

    Fix signOut callback parameter in LogoutButton component

--- src/components/_buttons/LogOut.tsx
+++ src/components/_buttons/LogOut.tsx
@@ -6,7 +6,7 @@ export default function LogoutButton() {
   return (
     <button
       className="cursor-pointer text-red-500 hover:bg-[#2a2a2a]"
-      onClick={() => signOut({ callbackUrl: "/" })}
+      onClick={() => signOut({ redirectTo: "/" })}
     >
       {" "}
       Logout
